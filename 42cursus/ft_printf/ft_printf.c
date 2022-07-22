1fa6a6af21a93173ed4ff1608f2fbfd2f446d9be
username <ehdbs8747@naver.com>
2022-07-22 17:51:51 +0900

    ADD ft_printf.c/parse_format, print_format.c \n MODIFY ft_printf.h

--- 42cursus/ft_printf/ft_printf.c
+++ 42cursus/ft_printf/ft_printf.c
@@ -6,11 +6,31 @@
 /*   By: doykim <doykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 17:03:32 by doykim            #+#    #+#             */
-/*   Updated: 2022/07/22 13:00:13 by doykim           ###   ########.fr       */
+/*   Updated: 2022/07/22 16:54:18 by doykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+int	parse_format(ap, char *format)
+{
+	if (*format == 'c')
+		return write(1, &va_arg(ap, int), 1);
+	else if (*format == 's')
+		return print_s(va_arg(ap, char *));
+	else if (*format == 'p')
+		return print_p(va_arg(ap, void *));
+	else if (*format == 'd' || *format == 'i')
+		return print_di(va_arg(ap, int));
+	else if (*format == 'u')
+		return print_u(va_arg(ap, unsigned int));
+	else if (*format == 'x' || *format == 'X')
+		return print_x(va_arg(ap, unsigned int))
+	else if (*format == '%')
+		return write(1, "%", 1);
+	else
+		return (-1)
+}
 
 int	ft_printf(const char *format, ...)
 {
@@ -22,7 +42,7 @@ int	ft_printf(const char *format, ...)
 	while (*format != '\0')
 	{
 		if (*format != '%')
-			len += ft_putchar_fd(*format++, 1);
+			len += write(1, *format++, 1);
 		else
 			len += parse_format(ap, (char *)format);
 	}
--- 42cursus/ft_printf/print_format.c
+++ 42cursus/ft_printf/print_format.c
@@ -1,19 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   print_format.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: doykim <doykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/17 17:05:11 by doykim            #+#    #+#             */
-/*   Updated: 2022/07/22 16:54:38 by doykim           ###   ########.fr       */
+/*   Created: 2022/07/22 15:55:22 by doykim            #+#    #+#             */
+/*   Updated: 2022/07/22 16:54:56 by doykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#include "ft_printf.h"
 
-# include <stdarg.h>
-# include "libft.h"
+int	print_s(char *s)
+{
+	ft_putstr_fd(s, 1);
+	return ft_strlen(s);
+}
 
-#endif
+int print_p(unsigned int)
+{
+	
+}
+
+int	print_di(int n)
+{
+	print_s(ft_itoa(n));	
+}
+
+int	print_u(unsigned int n)
+{
+
+}
+
+int print_x(unsigned int n)
+{
+
+}
+
