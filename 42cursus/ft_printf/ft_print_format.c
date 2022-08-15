c6dd3f14045feddde6da4adb2e09d6340af3d859
ehdbs0903 <ehdbs8747@naver.com>
2022-08-15 18:59:13 +0900

    Modify ft_print_format.c

--- 42cursus/ft_printf/ft_print_format.c
+++ 42cursus/ft_printf/ft_print_format.c
@@ -1,40 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   print_format.c                                     :+:      :+:    :+:   */
+/*   ft_print_format.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: doykim <doykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 15:55:22 by doykim            #+#    #+#             */
-/*   Updated: 2022/07/22 16:54:56 by doykim           ###   ########.fr       */
+/*   Updated: 2022/08/15 18:50:31 by doykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	print_s(char *s)
+int	ft_print_c(char c)
+{
+	return write(1, &c, 1);
+}
+
+int	ft_print_s(char *s)
 {
 	ft_putstr_fd(s, 1);
 	return ft_strlen(s);
 }
 
-int print_p(unsigned int)
+int	ft_print_di(int n)
 {
-	
+	return ft_print_s(ft_itoa(n));	
 }
-
-int	print_di(int n)
-{
-	print_s(ft_itoa(n));	
-}
-
-int	print_u(unsigned int n)
-{
-
-}
-
-int print_x(unsigned int n)
-{
-
-}
-
