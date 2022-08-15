d84aba93b6ee4fcf5994c33f7f75b79ccbe85627
ehdbs0903 <ehdbs8747@naver.com>
2022-08-15 21:07:11 +0900

    Modify ft_print_format.c

--- 42cursus/ft_printf/ft_print_format.c
+++ 42cursus/ft_printf/ft_print_format.c
@@ -6,7 +6,7 @@
 /*   By: doykim <doykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 15:55:22 by doykim            #+#    #+#             */
-/*   Updated: 2022/08/15 18:50:31 by doykim           ###   ########.fr       */
+/*   Updated: 2022/08/15 21:03:23 by doykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,19 @@ int	ft_print_c(char c)
 
 int	ft_print_s(char *s)
 {
+	if (!s)
+		return write(1, "(null)", 6);
 	ft_putstr_fd(s, 1);
 	return ft_strlen(s);
 }
 
 int	ft_print_di(int n)
 {
-	return ft_print_s(ft_itoa(n));	
+	int		len;
+	char	*s;
+	
+	s = ft_itoa(n);
+	len = ft_print_s(s);
+	free(s);
+	return len;
 }
