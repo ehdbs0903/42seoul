190c11e739c3a624106dcbe04b73e91dbaa6259f
ehdbs0903 <ehdbs8747@naver.com>
2022-08-15 21:08:33 +0900

    Modify ft_printf.c

--- 42cursus/ft_printf/ft_printf.c
+++ 42cursus/ft_printf/ft_printf.c
@@ -6,45 +6,48 @@
 /*   By: doykim <doykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 17:03:32 by doykim            #+#    #+#             */
-/*   Updated: 2022/08/15 18:47:23 by doykim           ###   ########.fr       */
+/*   Updated: 2022/08/15 20:31:28 by doykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_parse_format(va_list ap, char *format)
+int	ft_parse_format(va_list *ap, char format)
 {
-	if (*format == 'c')
-		return ft_print_c(va_arg(ap, int));
-	else if (*format == 's')
-		return ft_print_s(va_arg(ap, char *));
-	else if (*format == 'p')
-		return ft_print_p(va_arg(ap, unsigned long long));
-	else if (*format == 'd' || *format == 'i')
-		return ft_print_di(va_arg(ap, int));
-	else if (*format == 'u')
-		return ft_print_u(va_arg(ap, unsigned int));
-	else if (*format == 'x' || *format == 'X')
-		return ft_print_x(va_arg(ap, unsigned int), *format);
-	else if (*format == '%')
+	if (format == 'c')
+		return ft_print_c(va_arg(*ap, int));
+	else if (format == 's')
+		return ft_print_s(va_arg(*ap, char *));
+	else if (format == 'p')
+		return ft_print_p(va_arg(*ap, unsigned long long));
+	else if (format == 'd' || format == 'i')
+		return ft_print_di(va_arg(*ap, int));
+	else if (format == 'u')
+		return ft_print_u(va_arg(*ap, unsigned int));
+	else if (format == 'x' || format == 'X')
+		return ft_print_x(va_arg(*ap, unsigned int), format);
+	else if (format == '%')
 		return write(1, "%", 1);
 	else
 		return (-1);
 }
 
-int	ft_printf(char *format, ...)
+int	ft_printf(const char *format, ...)
 {
 	va_list	ap;
 	int		len;
+	int		i;
 
+	i = 0;
 	len = 0;
 	va_start(ap, format);
-	while (*format != '\0')
+	while (format[i])
 	{
-		if (*format != '%')
-			len += write(1, format++, 1);
+		if (format[i] != '%')
+			len += write(1, &format[i], 1);
 		else
-			len += ft_parse_format(ap, (char *)format);
+			len += ft_parse_format(&ap, format[++i]);
+		i++;
 	}
 	va_end(ap);
 	if (len < 0)
