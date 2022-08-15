910cf80d4233dd7ddcbb60567369b3e519a531a4
ehdbs0903 <ehdbs8747@naver.com>
2022-08-15 18:54:46 +0900

    Modify ft_printf.h

--- 42cursus/ft_printf/ft_printf.h
+++ 42cursus/ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: doykim <doykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 17:05:11 by doykim            #+#    #+#             */
-/*   Updated: 2022/07/22 16:54:38 by doykim           ###   ########.fr       */
+/*   Updated: 2022/08/15 18:48:39 by doykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,22 @@
 # define FT_PRINTF_H
 
 # include <stdarg.h>
-# include "libft.h"
+# include "libft/libft.h"
+
+int		ft_printf(char *format, ...);
+int		ft_parse_format(va_list ap, char *format);
+int		ft_print_c(char c);
+int		ft_print_s(char *s);
+int		ft_print_di(int n);
+int		ft_print_p(unsigned long long ptr);
+int		ft_print_u(unsigned int n);
+int		ft_print_x(unsigned int num, char format);
+void	ft_putstr(char *str);
+void	ft_put_ptr(uintptr_t num);
+int		ft_ptr_len(uintptr_t num);
+char	*ft_uitoa(unsigned int n);
+int		ft_num_len(unsigned	int num);
+void	ft_put_hex(unsigned int num, char format);
+int		ft_hex_len(unsigned	int num);
 
 #endif
