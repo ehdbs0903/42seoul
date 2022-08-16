e1c65c68ec7eee6458bb8a9b639b518eb143b848
username <ehdbs8747@naver.com>
2022-08-16 12:25:18 +0900

    Add ft_print_format.c

--- 42cursus/ft_printf/ft_print_format.c
+++ 42cursus/ft_printf/ft_print_format.c
@@ -6,7 +6,7 @@
 /*   By: doykim <doykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 15:55:22 by doykim            #+#    #+#             */
-/*   Updated: 2022/08/15 21:03:23 by doykim           ###   ########.fr       */
+/*   Updated: 2022/08/16 12:14:28 by doykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,24 @@
 
 int	ft_print_c(char c)
 {
-	return write(1, &c, 1);
+	return (write(1, &c, 1));
 }
 
 int	ft_print_s(char *s)
 {
 	if (!s)
-		return write(1, "(null)", 6);
+		return (write(1, "(null)", 6));
 	ft_putstr_fd(s, 1);
-	return ft_strlen(s);
+	return (ft_strlen(s));
 }
 
 int	ft_print_di(int n)
 {
 	int		len;
 	char	*s;
-	
+
 	s = ft_itoa(n);
 	len = ft_print_s(s);
 	free(s);
-	return len;
+	return (len);
 }
