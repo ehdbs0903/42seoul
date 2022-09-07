50464436c2640379eb4e498f7aa70626eb500ffe
username <ehdbs8747@naver.com>
2022-09-07 15:05:49 +0900

    Update 42cursus/get_next_line/get_next_line.h

--- 42cursus/get_next_line/get_next_line.h
+++ 42cursus/get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: doykim <daykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/15 21:49:08 by doykim            #+#    #+#             */
-/*   Updated: 2022/08/15 21:50:14 by doykim           ###   ########.fr       */
+/*   Updated: 2022/09/07 15:01:38 by doykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,11 @@
 # define GET_NEXT_LINE_H
 
 # include <stdlib.h>
+# include <unistd.h>
+
+int					get_next_line(int fd, char **line);
+int					has_return(char *str);
+size_t				ft_strlen(const char *s);
+char				*join_str(const char *s1, const char *s2);
 
 #endif
