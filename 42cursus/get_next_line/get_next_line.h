0db6422937a0e1efc3e90cedfd52250fa435bfe4
username <ehdbs8747@naver.com>
2022-09-07 16:27:12 +0900

    Update 42cursus/get_next_line/get_next_line.h

--- 42cursus/get_next_line/get_next_line.h
+++ 42cursus/get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: doykim <daykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/15 21:49:08 by doykim            #+#    #+#             */
-/*   Updated: 2022/09/07 15:01:38 by doykim           ###   ########.fr       */
+/*   Updated: 2022/09/07 15:14:24 by doykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,16 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-int					get_next_line(int fd, char **line);
-int					has_return(char *str);
-size_t				ft_strlen(const char *s);
-char				*join_str(const char *s1, const char *s2);
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_read_to_left_str(int fd, char *left_str);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *left_str, char *buff);
+size_t	ft_strlen(char *s);
+char	*ft_get_line(char *left_str);
+char	*ft_new_left_str(char *left_str);
 
 #endif
