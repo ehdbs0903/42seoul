aeb26c1959b60208073422285a1c28bd687e6700
username <ehdbs8747@naver.com>
2022-09-07 16:27:03 +0900

    Update 42cursus/get_next_line/get_next_line.c

--- 42cursus/get_next_line/get_next_line.c
+++ 42cursus/get_next_line/get_next_line.c
@@ -6,86 +6,47 @@
 /*   By: doykim <daykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/15 21:37:29 by doykim            #+#    #+#             */
-/*   Updated: 2022/09/07 15:00:52 by doykim           ###   ########.fr       */
+/*   Updated: 2022/09/07 15:16:11 by doykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_save(char *save)
+char	*ft_read_to_left_str(int fd, char *left_str)
 {
-	char	*rtn;
-	int		i;
-	int		j;
+	char	*buff;
+	int		rd_bytes;
 
-	i = 0;
-	j = 0;
-	if (!save)
-		return (0);
-	while (save[i] && save[i] != '\n')
-		i++;
-	if (!save[i])
+	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buff)
+		return (NULL);
+	rd_bytes = 1;
+	while (!ft_strchr(left_str, '\n') && rd_bytes != 0)
 	{
-		free(save);
-		return (0);
-	}
-	if (!(rtn = malloc(sizeof(char) * ((ft_strlen(save) - i) + 1))))
-		return (0);
-	i++;
-	while (save[i])
-		rtn[j++] = save[i++];
-	rtn[j] = '\0';
-	free(save);
-	return (rtn);
-}
-
-char	*get_line(char *str)
-{
-	int		i;
-	char	*rtn;
-
-	i = 0;
-	if (!str)
-		return (0);
-	while (str[i] && str[i] != '\n')
-		i++;
-	if (!(rtn = malloc(sizeof(char) * (i + 1))))
-		return (0);
-	i = 0;
-	while (str[i] && str[i] != '\n')
-	{
-		rtn[i] = str[i];
-		i++;
-	}
-	rtn[i] = '\0';
-	return (rtn);
-}
-
-int		get_next_line(int fd, char **line)
-{
-	char			*buff;
-	static char		*save;
-	int				reader;
-
-	reader = 1;
-	if (fd < 0 || !line || BUFFER_SIZE <= 0)
-		return (-1);
-	if (!(buff = malloc(sizeof(char) * (BUFFER_SIZE + 1))))
-		return (-1);
-	while (!has_return(save) && reader != 0)
-	{
-		if ((reader = read(fd, buff, BUFFER_SIZE)) == -1)
+		rd_bytes = read(fd, buff, BUFFER_SIZE);
+		if (rd_bytes == -1)
 		{
 			free(buff);
-			return (-1);
+			return (NULL);
 		}
-		buff[reader] = '\0';
-		save = join_str(save, buff);
+		buff[rd_bytes] = '\0';
+		left_str = ft_strjoin(left_str, buff);
 	}
 	free(buff);
-	*line = get_line(save);
-	save = get_save(save);
-	if (reader == 0)
+	return (left_str);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	static char	*left_str;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (0);
-	return (1);
-}#
+	left_str = ft_read_to_left_str(fd, left_str);
+	if (!left_str)
+		return (NULL);
+	line = ft_get_line(left_str);
+	left_str = ft_new_left_str(left_str);
+	return (line);
+}
