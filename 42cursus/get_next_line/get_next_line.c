eb95d48a1fd98afc99517daa170e73d6bf81c243
username <ehdbs8747@naver.com>
2022-09-11 23:19:04 +0900

    Update 42cursus/get_next_line/get_next_line.c

--- 42cursus/get_next_line/get_next_line.c
+++ 42cursus/get_next_line/get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: doykim <daykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/15 21:37:29 by doykim            #+#    #+#             */
-/*   Updated: 2022/09/07 15:16:11 by doykim           ###   ########.fr       */
+/*   Updated: 2022/09/11 23:11:22 by doykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_read_to_left_str(int fd, char *left_str)
+char	*ft_read_to_str(int fd, char *str)
 {
 	char	*buff;
 	int		rd_bytes;
@@ -21,7 +21,7 @@ char	*ft_read_to_left_str(int fd, char *left_str)
 	if (!buff)
 		return (NULL);
 	rd_bytes = 1;
-	while (!ft_strchr(left_str, '\n') && rd_bytes != 0)
+	while (!ft_strchr(str, '\n') && rd_bytes != 0)
 	{
 		rd_bytes = read(fd, buff, BUFFER_SIZE);
 		if (rd_bytes == -1)
@@ -30,23 +30,23 @@ char	*ft_read_to_left_str(int fd, char *left_str)
 			return (NULL);
 		}
 		buff[rd_bytes] = '\0';
-		left_str = ft_strjoin(left_str, buff);
+		str = ft_strjoin(str, buff);
 	}
 	free(buff);
-	return (left_str);
+	return (str);
 }
 
 char	*get_next_line(int fd)
 {
 	char		*line;
-	static char	*left_str;
+	static char	*str;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (0);
-	left_str = ft_read_to_left_str(fd, left_str);
-	if (!left_str)
+	str = ft_read_to_str(fd, str);
+	if (!str)
 		return (NULL);
-	line = ft_get_line(left_str);
-	left_str = ft_new_left_str(left_str);
+	line = ft_get_line(str);
+	str = ft_new_str(str);
 	return (line);
 }
