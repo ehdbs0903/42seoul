59d1df5457ab6686428d75eccc7bea9fd75426ae
ehdbs0903 <ehdbs8747@naver.com>
2022-09-14 23:31:16 +0900

    Add minitalk/ft_minitalk.h

--- 42cursus/InnerCircle/circle2/minitalk/ft_minitalk.h
+++ 42cursus/InnerCircle/circle2/minitalk/ft_minitalk.h
@@ -6,10 +6,7 @@
 /*   By: doykim <daykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 22:02:35 by doykim            #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2022/09/13 23:18:50 by doykim           ###   ########.fr       */
-=======
->>>>>>> 2cd21be9b4193e46dab2969a66d26067cf479769
+/*   Updated: 2022/09/13 23:30:21 by doykim           ###   ########.fr       */
 /*   Updated: 2022/09/13 22:05:26 by doykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
@@ -17,13 +14,7 @@
 #ifndef FT_MINITALK_H
 # define FT_MINITALK_H
 
-<<<<<<< HEAD
 # include <signal.h>
 # include "ft_printf/ft_printf.h"
-=======
-# include <unistd.h>
-# include <signal.h>
-# include <stdlib.h>
->>>>>>> 2cd21be9b4193e46dab2969a66d26067cf479769
 
 #endif
