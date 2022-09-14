73648e2ecc6209c576c7eeff2f790dd48d203815
ehdbs0903 <ehdbs8747@naver.com>
2022-09-14 23:31:04 +0900

    Add minitalk/ft_clinet.c

--- 42cursus/InnerCircle/circle2/minitalk/ft_client.c
+++ 42cursus/InnerCircle/circle2/minitalk/ft_client.c
@@ -6,13 +6,22 @@
 /*   By: doykim <daykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 22:00:45 by doykim            #+#    #+#             */
-/*   Updated: 2022/09/13 22:01:15 by doykim           ###   ########.fr       */
+/*   Updated: 2022/09/13 23:41:17 by doykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_minitalk.h"
 
-int main(void)
+int main(int argc, char **argv)
 {
-	
+	int pid;
+
+	pid = getpid();
+	ft_printf("client pid: %d\n", pid);
+	while(1)
+	{
+		ft_printf("running\n");
+		sleep(5);
+	}
+	return (0);
 }
