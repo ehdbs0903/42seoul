a39d99ec61bfc13292166edd52abe7f437683e5f
username <ehdbs8747@naver.com>
2022-09-20 08:21:04 +0900

    Update minitalk/ft_client.c

--- 42cursus/InnerCircle/circle2/minitalk/ft_client.c
+++ 42cursus/InnerCircle/circle2/minitalk/ft_client.c
@@ -6,22 +6,81 @@
 /*   By: doykim <daykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 22:00:45 by doykim            #+#    #+#             */
-/*   Updated: 2022/09/13 23:41:17 by doykim           ###   ########.fr       */
+/*   Updated: 2022/09/20 08:19:51 by doykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_minitalk.h"
 
-int main(int argc, char **argv)
+void	signal_error(void)
 {
-	int pid;
+	ft_printf("\nclient: unexpected error.\n");
+	exit(EXIT_FAILURE);
+}
 
-	pid = getpid();
-	ft_printf("client pid: %d\n", pid);
-	while(1)
+void	char_to_bin(unsigned char c, int pid)
+{
+	int	bit;
+
+	bit = 0;
+	while (bit < 8)
 	{
-		ft_printf("running\n");
-		sleep(5);
+		if (c & 128)
+		{
+			if (kill(pid, SIGUSR2) == -1)
+				signal_error();
+		}
+		else
+		{
+			if (kill(pid, SIGUSR1) == -1)
+				signal_error();
+		}
+		c <<= 1;
+		bit++;
+		pause();
+		usleep(100);
 	}
-	return (0);
+}
+
+void	sent_text(char *str, int pid)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		char_to_bin(str[i++], pid);
+	char_to_bin('\0', pid);
+}
+
+void	recieved(int sig)
+{
+	static int	sent;
+
+	if (sig == SIGUSR1)
+	{
+		ft_printf("%s%d signal sent successfully!%s\n", ++sent);
+		exit(EXIT_SUCCESS);
+	}
+	if (sig == SIGUSR2)
+		++sent;
+}
+
+int main(int ac, char **av)
+{
+	int	server_pid;
+	int	client_pid;
+
+	client_pid = getpid();
+	if (ac == 3)
+	{
+		ft_printf("client pid: %d\n", client_pid);
+		signal(SIGUSR1, recieved);
+		signal(SIGUSR2, recieved);
+		server_pid = ft_atoi(av[1]);
+		ft_printf("Text currently sending.. \n");
+		sent_text(av[2], server_pid);
+	}
+	else
+		ft_printf("usage: ./client <server_pid> <text to send>\n");
+	return (EXIT_FAILURE);
 }
