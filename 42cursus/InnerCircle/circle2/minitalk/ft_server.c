a678af42b084c8e3cc91c74a6913ca6494aabed1
username <ehdbs8747@naver.com>
2022-09-20 08:21:32 +0900

    Update minitalk/ft_server.c

--- 42cursus/InnerCircle/circle2/minitalk/ft_server.c
+++ 42cursus/InnerCircle/circle2/minitalk/ft_server.c
@@ -6,19 +6,79 @@
 /*   By: doykim <daykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 21:59:16 by doykim            #+#    #+#             */
-/*   Updated: 2022/09/13 23:51:49 by doykim           ###   ########.fr       */
+/*   Updated: 2022/09/20 08:19:15 by doykim           ###   ########.fr       */
 /*   Updated: 2022/09/13 22:11:25 by doykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_minitalk.h"
 
-int	main(int argc, char **argv)
+void	signal_error(void)
+{
+	ft_printf("\nserver: unexpected error.\n");
+	exit(EXIT_FAILURE);
+}
+
+void	extended_action(char *c, int *received, int *client_pid, int *bit)
+{
+	ft_printf("%c", *c);
+	if (*c == '\0')
+	{
+		ft_printf("\n%d signal recieved from client PID: %d\n",
+			*received, *client_pid);
+		*received = 0;
+		*c = 0;
+		if (kill(*client_pid, SIGUSR1) == -1)
+			signal_error();
+		return ;
+	}
+	*bit = 0;
+}
+
+void	action(int sig, siginfo_t *info, void *context)
+{
+	static int	client_pid;
+	static int	bit;
+	static char	c;
+	static int	received;
+	static int	current_pid;
+
+	(void)context;
+	if (!client_pid)
+		client_pid = info->si_pid;
+	current_pid = info->si_pid;
+	if (client_pid != current_pid)
+	{
+		client_pid = current_pid;
+		bit = 0;
+		c = 0;
+		received = 0;
+	}
+	c |= (sig == SIGUSR2);
+	received++;
+	bit++;
+	if (bit == 8)
+		extended_action(&c, &received, &client_pid, &bit);
+	c <<= 1;
+	usleep(100);
+	kill(client_pid, SIGUSR2);
+}
+
+int	main(void)
 {
 	int	pid;
+	struct sigaction act;
 
 	pid = getpid();
 	ft_printf("server pid: %d\n", pid);
-	kill(ft_atoi(argv[1]), SIGKILL);
-	return (0);
+	act.sa_sigaction = action;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = SA_SIGINFO;
+	while (1)
+	{
+		sigaction(SIGUSR1, &act, 0);
+		sigaction(SIGUSR2, &act, 0);
+		pause();
+	}
+	return (EXIT_FAILURE);
 }
