7810834cc3f8a9fe55d575e61f5ecd68f7e85789
ehdbs0903 <ehdbs8747@naver.com>
2022-10-05 15:23:20 +0900

    Add so_long/ft_so_long.h

---
+++ 42cursus/InnerCircle/circle2/so_long/ft_so_long.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_so_long.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: doykim <daykim@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/29 16:44:29 by doykim            #+#    #+#             */
+/*   Updated: 2022/09/29 19:27:34 by doykim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_SO_LONG_H
+# define FT_SO_LONG_H
+
+# include "mlx/mlx.h"
+# include "ft_printf/ft_printf.h"
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+#endif
