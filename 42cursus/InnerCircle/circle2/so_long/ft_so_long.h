feeb0b625709869efcf8881c02ad78a4b9e6c8d8
username <ehdbs8747@naver.com>
2022-10-05 21:28:52 +0900

    Update so_long/ft_so_long.h

--- 42cursus/InnerCircle/circle2/so_long/ft_so_long.h
+++ 42cursus/InnerCircle/circle2/so_long/ft_so_long.h
@@ -6,7 +6,7 @@
 /*   By: doykim <daykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 16:44:29 by doykim            #+#    #+#             */
-/*   Updated: 2022/09/29 19:27:34 by doykim           ###   ########.fr       */
+/*   Updated: 2022/10/05 21:27:26 by doykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,26 @@
 
 # include "mlx/mlx.h"
 # include "ft_printf/ft_printf.h"
+# include "get_next_line/get_next_line.h"
 # include <fcntl.h>
-# include <stdio.h>
-# include <stdlib.h>
+
+typedef struct s_game
+{
+	int		width;
+	int		height;
+	char	*map;
+	void	*mlx;
+	void	*win;
+	void	*grass;
+	void	*rock;
+	void	*blue_orb;
+	void	*red_orb;
+}	t_game;
+
+void	print_error();
+int		check_argument(int ac, char **av);
+void	init_img(t_game *game);
+void	set_img(t_game *game);
+void	read_map(char *filename, t_game *game);
 
 #endif
