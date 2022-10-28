0476825e3af92eec0a30cb5ba7968888edb466b2
username <ehdbs8747@naver.com>
2022-10-28 19:48:23 +0900

    Add so_long/ft_so_long.h

--- 42cursus/InnerCircle/circle2/so_long/ft_so_long.h
+++ 42cursus/InnerCircle/circle2/so_long/ft_so_long.h
@@ -6,7 +6,7 @@
 /*   By: doykim <daykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 16:44:29 by doykim            #+#    #+#             */
-/*   Updated: 2022/10/28 01:37:33 by doykim           ###   ########.fr       */
+/*   Updated: 2022/10/28 18:45:35 by doykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,15 @@
 # include "ft_printf/ft_printf.h"
 # include "get_next_line/get_next_line.h"
 # include <fcntl.h>
+
+# define X_EVENT_KEY_PRESS			2
+# define X_EVENT_KEY_RELEASE		3
+
+# define KEY_ESC	53
+# define KEY_W		13
+# define KEY_A		0
+# define KEY_S		1
+# define KEY_D		2
 
 typedef struct s_player
 {
@@ -32,6 +41,10 @@ typedef struct s_game
 {
 	int			width;
 	int			height;
+	int			c_cnt;
+	int			e_cnt;
+	int			p_cnt;
+	int			e_flag;
 	char		*map;
 	void		*mlx;
 	void		*win;
@@ -39,14 +52,16 @@ typedef struct s_game
 	void		*wall;
 	void		*collect;
 	void		*exit;
-	void		*o_exit;
 	t_player	player;
 }	t_game;
 
-void	print_error();
+void	print_error(int n);
 int		check_argument(int ac, char **av);
 void	init_img(t_game *game);
 void	set_map_img(t_game *game);
 void	read_map(char *filename, t_game *game);
+int		key_press(int keycode, t_game *game);
+void	check_map(t_game *game);
+void	put_img(t_game *game, void *img, int w, int h);
 
 #endif
