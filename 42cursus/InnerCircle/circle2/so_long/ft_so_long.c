d58b9da1324fcba02271f4679ca3c9d9802f5d95
username <ehdbs8747@naver.com>
2022-10-28 19:48:16 +0900

    Add so_long/ft_so_long.c

--- 42cursus/InnerCircle/circle2/so_long/ft_so_long.c
+++ 42cursus/InnerCircle/circle2/so_long/ft_so_long.c
@@ -6,16 +6,16 @@
 /*   By: doykim <daykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 16:43:37 by doykim            #+#    #+#             */
-/*   Updated: 2022/10/28 01:37:29 by doykim           ###   ########.fr       */
+/*   Updated: 2022/10/28 18:45:30 by doykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_so_long.h"
 
-void	print_error()
+void	print_error(int n)
 {
 	ft_printf("Error");
-	exit(1);
+	exit(n);
 }
 
 int	check_argument(int ac, char **av)
@@ -36,20 +36,27 @@ void	init_game(t_game *game)
 {
 	game->width = 0;
 	game->height = 0;
+	game->c_cnt = 0;
+	game->e_cnt = 0;
+	game->p_cnt = 0;
+	game->e_flag = 0;
+	init_img(game);
 }
 
 int	main(int ac, char **av)
 {
 	t_game	game;
-	
+
 	if (check_argument(ac, av) == -1)
-		print_error();
-	//init_game(&game);
+		print_error(1);
 	game.mlx = mlx_init();
+	game.win = mlx_new_window(game.mlx, game.width * 50, game.height * 50,
+			"mlx 42");
+	init_game(&game);
 	read_map(av[1], &game);
-	game.win = mlx_new_window(game.mlx, game.width * 50, game.height * 50, "mlx 42");
 	init_img(&game);
 	set_map_img(&game);
-	mlx_loop(game.mlx);	
+	mlx_hook(game.win, X_EVENT_KEY_RELEASE, 0, &key_press, &game);
+	mlx_loop(game.mlx);
 	return (0);
 }
