b81e5b54221abf4fbdc9cc28bbf0e86a1b70f908
username <ehdbs8747@naver.com>
2022-10-30 17:47:46 +0900

    Update so_long/ft_so_long_utils.c

--- 42cursus/InnerCircle/circle2/so_long/ft_so_long_utils.c
+++ 42cursus/InnerCircle/circle2/so_long/ft_so_long_utils.c
@@ -6,7 +6,7 @@
 /*   By: doykim <doykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 16:23:03 by doykim            #+#    #+#             */
-/*   Updated: 2022/10/28 18:45:32 by doykim           ###   ########.fr       */
+/*   Updated: 2022/10/30 17:46:08 by doykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,6 @@ void	init_img(t_game *game)
 			&width, &height);
 	game->exit = mlx_xpm_file_to_image(game->mlx, "./img/exit.xpm", &width,
 			&height);
-	//game->o_exit = mlx_xpm_file_to_image(game->mlx, "./img/o_exit.xpm", &width,
-	//		&height);
 	game->player.front = mlx_xpm_file_to_image(game->mlx, "./img/front.xpm",
 			&width, &height);
 	game->player.back = mlx_xpm_file_to_image(game->mlx, "./img/back.xpm",
@@ -34,6 +32,8 @@ void	init_img(t_game *game)
 	game->player.left = mlx_xpm_file_to_image(game->mlx, "./img/left.xpm",
 			&width, &height);
 	game->player.right = mlx_xpm_file_to_image(game->mlx, "./img/right.xpm",
+			&width, &height);
+	game->monster = mlx_xpm_file_to_image(game->mlx, "./img/monster.xpm",
 			&width, &height);
 }
 
@@ -57,10 +57,12 @@ void	set_map_img(t_game *game)
 				put_img(game, game->player.front, w, h);
 			else if (game->map[h * game->width + w] == 'E')
 				put_img(game, game->exit, w, h);
+			else if (game->map[h * game->width + w] == 'M')
+				put_img(game, game->monster, w, h);
 			else if (game->map[h * game->width + w] == '0')
 				continue ;
 			else
-				print_error(1);
+				exit_game(1);
 		}
 	}
 }
@@ -69,10 +71,11 @@ void	read_map(char *filename, t_game *game)
 {
 	int		fd;
 	char	*line;
+	char	*temp;
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		print_error(1);
+		exit_game(1);
 	line = get_next_line(fd);
 	game->height = 0;
 	game->width = ft_strlen(line) - 1;
@@ -84,13 +87,30 @@ void	read_map(char *filename, t_game *game)
 		line = get_next_line(fd);
 		if (line)
 		{
-			game->map = ft_strjoin(game->map, line);
+			temp = ft_strjoin(game->map, line);
+			free(game->map);
+			game->map = temp;
 			game->map[ft_strlen(game->map) - 1] = 0;
+			free(line);
 		}
 	}
 	close(fd);
 	check_map(game);
 	ft_printf("%s\n", game->map);
+}
+
+void	count_pce(t_game *game, int h, int w)
+{
+	if (game->map[h * game -> width + w] == 'P')
+	{
+		game->p_cnt++;
+		game->player.x = w;
+		game->player.y = h;
+	}
+	else if (game->map[h * game -> width + w] == 'C')
+		game->c_cnt++;
+	else if (game->map[h * game -> width + w] == 'E')
+		game->e_cnt++;
 }
 
 void	check_map(t_game *game)
@@ -104,23 +124,15 @@ void	check_map(t_game *game)
 		w = -1;
 		while (++w < game->width)
 		{	
-			if ((h == 0 || h == game->height - 1) && game->map[h * game->width + w] != '1')
-				print_error(1);
-			if ((w == 0 || w == game->width - 1) && game->map[h * game->width + w] != '1')
-				print_error(1);
-			if (game->map[h * game -> width + w] == 'P')
-			{
-				game->p_cnt++;
-				game->player.x = w;
-				game->player.y = h;
-			}
-			else if (game->map[h * game -> width + w] == 'C')
-				game->c_cnt++;
-			else if (game->map[h * game -> width + w] == 'E')
-				game->e_cnt++;
+			if ((h == 0 || h == game->height - 1)
+				&& game->map[h * game->width + w] != '1')
+				exit_game(1);
+			if ((w == 0 || w == game->width - 1)
+				&& game->map[h * game->width + w] != '1')
+				exit_game(1);
+			count_pce(game, h, w);
 		}
 	}
-	if (game->c_cnt != 1 || !game->p_cnt || !game->e_cnt)
-		print_error(1);
-	game->c_cnt = c;
+	if (game->p_cnt != 1 || !game->c_cnt || !game->e_cnt)
+		exit_game(1);
 }
