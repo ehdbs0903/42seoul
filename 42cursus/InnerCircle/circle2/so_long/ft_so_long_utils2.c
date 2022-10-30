21721994bc252d3be96860d381f74ca7e8e6cac3
username <ehdbs8747@naver.com>
2022-10-30 17:47:51 +0900

    Update so_long/ft_so_long_utils2.c

--- 42cursus/InnerCircle/circle2/so_long/ft_so_long_utils2.c
+++ 42cursus/InnerCircle/circle2/so_long/ft_so_long_utils2.c
@@ -6,140 +6,64 @@
 /*   By: doykim <doykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 01:36:58 by doykim            #+#    #+#             */
-/*   Updated: 2022/10/28 18:45:33 by doykim           ###   ########.fr       */
+/*   Updated: 2022/10/30 17:46:10 by doykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_so_long.h"
 
-void	move_w(t_game *game)
+void	move(t_game *game, t_player player, int dy, int dx)
 {
-	int w;
-	int h;
-
-	h = game->player.y;
-	w = game->player.x;
-	if (game->map[(h - 1) * game->width + w] != '1')
-	{
-		mlx_put_image_to_window(game->mlx, game->win, game->floor,
-				game->player.x * 50, game->player.y * 50);
-		game->player.y--;
-		if (game->map[(h - 1) * game->width + w] == 'C')
+	if (game->map[(player.y + dy) * game->width + player.x + dx] != '1')
+	{	
+		if (game->map[(player.y + dy) * game->width + player.x + dx] == 'M')
+			exit_game(2);
+		if (game->map[(player.y + dy) * game->width + player.x + dx] == 'C')
 		{
 			game->c_cnt -= 1;
 			if (!game->c_cnt)
 				game->e_flag = 1;
 		}
-		if (game->map[(h - 1) * game->width + w] == 'E')
-			if (game->e_flag)
-			{
-				ft_printf("Sucess");
-				exit(0);
-			}
-		mlx_put_image_to_window(game->mlx, game->win, game->floor,
-				game->player.x * 50, game->player.y * 50);
-		mlx_put_image_to_window(game->mlx, game->win, game->player.back,
-				game->player.x * 50, game->player.y * 50);
-	}
-}
-
-void	move_s(t_game *game)
-{
-	int w;
-	int h;
-
-	h = game->player.y;
-	w = game->player.x;
-	if (game->map[(h + 1) * game->width + w] != '1')
-	{
-		mlx_put_image_to_window(game->mlx, game->win, game->floor, game->player.x * 50, game->player.y * 50);
-		game->player.y++;
-		if (game->map[(h + 1) * game->width + w] == 'C')
+		if (game->map[(player.y + dy) * game->width + player.x + dx] == 'E')
 		{
-			game->c_cnt -= 1;
-			if (!game->c_cnt)
-				game->e_flag = 1;
-		}
-		if (game->map[(h + 1) * game->width + w] == 'E')
 			if (game->e_flag)
-			{
-				ft_printf("Sucess");
-				exit(0);
-			}
-		mlx_put_image_to_window(game->mlx, game->win, game->floor, game->player.x * 50, game->player.y * 50);
-		mlx_put_image_to_window(game->mlx, game->win, game->player.front, game->player.x * 50, game->player.y * 50);
-	}
-}
-
-void	move_a(t_game *game)
-{
-	int w;
-	int h;
-
-	h = game->player.y;
-	w = game->player.x;
-	if (game->map[h * game->width + w - 1] != '1')
-	{
-		mlx_put_image_to_window(game->mlx, game->win, game->floor, game->player.x * 50, game->player.y * 50);
-		game->player.x--;
-		if (game->map[h * game->width + w - 1] == 'C')
-		{
-			game->c_cnt -= 1;
-			if (!game->c_cnt)
-				game->e_flag = 1;
+				exit_game(0);
+			else
+				return ;
 		}
-		if (game->map[h * game->width + w - 1] == 'E')
-			if (game->e_flag)
-			{
-				ft_printf("Sucess");
-				exit(0);
-			}
-		mlx_put_image_to_window(game->mlx, game->win, game->floor, game->player.x * 50, game->player.y * 50);
-		mlx_put_image_to_window(game->mlx, game->win, game->player.left, game->player.x * 50, game->player.y * 50);
-	}
-}
-
-void	move_d(t_game *game)
-{
-	int w;
-	int h;
-
-	h = game->player.y;
-	w = game->player.x;
-	if (game->map[h * game->width + w + 1] != '1')
-	{
-		mlx_put_image_to_window(game->mlx, game->win, game->floor, game->player.x * 50, game->player.y * 50);
-		game->player.x++;
-		if (game->map[h * game->width + w + 1] == 'C')
-		{
-			game->c_cnt -= 1;
-			if (!game->c_cnt)
-				game->e_flag = 1;
-		}
-		if (game->map[h * game->width + w + 1] == 'E')
-			if (game->e_flag)
-			{
-				ft_printf("Sucess");
-				exit(0);
-			}
-		mlx_put_image_to_window(game->mlx, game->win, game->floor, game->player.x * 50, game->player.y * 50);
-		mlx_put_image_to_window(game->mlx, game->win, game->player.right, game->player.x * 50, game->player.y * 50);
+		put_img(game, game->floor, player.x, player.y);
+		put_img(game, game->floor, player.x + dx, player.y + dy);
+		put_img(game, game->player.back, player.x + dx, player.y + dy);
+		game->player.x += dx;
+		game->player.y += dy;
 	}
 }
 
 int	key_press(int keycode, t_game *game)
 {
-	ft_printf("%d\n", keycode);
-	if (keycode == KEY_W || keycode == KEY_S || keycode == KEY_A || keycode == KEY_D)
+	if (keycode == KEY_W || keycode == KEY_S
+		|| keycode == KEY_A || keycode == KEY_D)
 	{
 		if (keycode == KEY_W)
-			move_w(game);
+		{
+			move(game, game->player, -1, 0);
+			put_img(game, game->player.back, game->player.x, game->player.y);
+		}
 		else if (keycode == KEY_S)
-			move_s(game);
+		{
+			move(game, game->player, 1, 0);
+			put_img(game, game->player.front, game->player.x, game->player.y);
+		}
 		else if (keycode == KEY_A)
-			move_a(game);
+		{
+			move(game, game->player, 0, -1);
+			put_img(game, game->player.left, game->player.x, game->player.y);
+		}
 		else if (keycode == KEY_D)
-			move_d(game);
+		{
+			move(game, game->player, 0, 1);
+			put_img(game, game->player.right, game->player.x, game->player.y);
+		}
 	}
 	else if (keycode == KEY_ESC)
 		exit(0);
