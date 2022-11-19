42b19537600e8de7f287fc0f83e5932d52b68e35
username <ehdbs8747@naver.com>
2022-11-19 21:08:01 +0900

    Update so_long/ft_so_utils.c

--- 42cursus/InnerCircle/circle2/so_long/ft_so_long_utils.c
+++ 42cursus/InnerCircle/circle2/so_long/ft_so_long_utils.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_so_long_utils3.c                                :+:      :+:    :+:   */
+/*   ft_so_long_utils.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: doykim <doykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 20:34:28 by doykim            #+#    #+#             */
-/*   Updated: 2022/10/28 20:36:35 by doykim           ###   ########.fr       */
+/*   Updated: 2022/11/01 16:06:34 by doykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,15 @@
 void	put_img(t_game *game, void *img, int w, int h)
 {
 	mlx_put_image_to_window(game->mlx, game->win, img, w * 50, h * 50);
+}
+
+void	draw_text(t_game *game)
+{
+	char	*temp;
+
+	put_img(game, game->wall, 0, 0);
+	temp = ft_itoa(game->m_cnt);
+	mlx_string_put(game->mlx, game->win, 10, 20, -1, temp);
+	free(temp);
+	ft_printf("move count: %d\n", game->m_cnt);
 }
