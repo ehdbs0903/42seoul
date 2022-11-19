0f36fb6102a6e2febab33e08f5d0d2660cd9c7ea
username <ehdbs8747@naver.com>
2022-11-19 21:07:45 +0900

    Update so_long/ft_so_long.c

--- 42cursus/InnerCircle/circle2/so_long/ft_so_long.c
+++ 42cursus/InnerCircle/circle2/so_long/ft_so_long.c
@@ -6,7 +6,7 @@
 /*   By: doykim <daykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 16:43:37 by doykim            #+#    #+#             */
-/*   Updated: 2022/10/30 16:50:23 by doykim           ###   ########.fr       */
+/*   Updated: 2022/11/01 16:35:58 by doykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,16 @@ void	exit_game(int n)
 		ft_printf("Success");
 		exit(0);
 	}
-	else
+	else if (n == -1)
 	{
 		ft_printf("Fail");
 		exit(0);
 	}
+	else
+		exit(0);
 }
 
-int	check_argument(int ac, char **av)
+static	int	check_argument(int ac, char **av)
 {
 	int	i;
 
@@ -45,15 +47,24 @@ int	check_argument(int ac, char **av)
 	return (0);
 }
 
-void	init_game(t_game *game)
+static	void	init_game(t_game *game)
 {
+	game->mlx = mlx_init();
 	game->width = 0;
 	game->height = 0;
 	game->c_cnt = 0;
 	game->e_cnt = 0;
 	game->p_cnt = 0;
+	game->m_cnt = 0;
 	game->e_flag = 0;
 	init_img(game);
+}
+
+static	int	x_exit(int n)
+{
+	n = 0;
+	exit(n);
+	return (0);
 }
 
 int	main(int ac, char **av)
@@ -62,13 +73,15 @@ int	main(int ac, char **av)
 
 	if (check_argument(ac, av) == -1)
 		exit_game(1);
-	game.mlx = mlx_init();
 	init_game(&game);
 	read_map(av[1], &game);
+	check_map(&game);
 	game.win = mlx_new_window(game.mlx, game.width * 50, game.height * 50,
 			"mlx 42");
 	set_map_img(&game);
-	mlx_hook(game.win, X_EVENT_KEY_RELEASE, 0, &key_press, &game);
+	mlx_string_put(game.mlx, game.win, 10, 20, -1, "0");
+	mlx_hook(game.win, 2, 0, key_press, &game);
+	mlx_hook(game.win, 17, 0, x_exit, &game);
 	mlx_loop(game.mlx);
 	return (0);
 }
