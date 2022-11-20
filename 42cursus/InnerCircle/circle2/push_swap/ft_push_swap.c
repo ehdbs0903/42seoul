e6c88d38d435e546fcc48cd0fd1f99d7b5532f65
username <ehdbs8747@naver.com>
2022-11-20 16:23:46 +0900

    Update push_swap/ft_push_swap.c

--- 42cursus/InnerCircle/circle2/push_swap/ft_push_swap.c
+++ 42cursus/InnerCircle/circle2/push_swap/ft_push_swap.c
@@ -6,55 +6,26 @@
 /*   By: doykim <doykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 12:57:43 by doykim            #+#    #+#             */
-/*   Updated: 2022/09/27 15:55:34 by doykim           ###   ########.fr       */
+/*   Updated: 2022/11/20 13:29:12 by doykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_push_swap.h"
-#include <stdio.h>
 
-int	check_error(int ac, char **av, t_list **stack)
+int	main(int argc, char *argv[])
 {
-	int		i;
-	int
-	t_list	*temp;
+	t_lists	*lists;
 
-	if (ac <= 1)
+	if (argc < 2)
 		return (0);
-	i = 1;
-	while (av[i])
-	{
-		
-		if (!ft_isdigit((int)av[i]))
-		{
-			printf("%d\n", (int)av[i]);
-			//if ft_isdigit == 0, stack clear
-			return (0);
-		}
-		temp = ft_lstnew((int)av[i]);
-		//if temp == 0, ft_lstnew return 0
-		ft_lstadd_back(stack, temp);
-
-	}
-	return (1);
-}
-
-int	main(int ac, char **av)
-{
-	static t_list	**stack;
-	t_list	*lst;
-	
-	if (!check_error(ac, av, stack))
-	{
-		printf("Error");
+	lists = (t_lists *)malloc(sizeof(t_lists));
+	if (!lists)
 		return (0);
-	}
-	lst = stack[0];
-	while (lst->next)
-	{
-		printf("%d ", lst->content);
-		lst = lst->next;
-	}
-	printf("\n");
+	lists->a = init_list();
+	lists->b = init_list();
+	check_input(argv, lists);
+	if (!(check_sorted(lists->a)))
+		sort_list(lists);
+	free_lists(lists);
 	return (0);
 }
