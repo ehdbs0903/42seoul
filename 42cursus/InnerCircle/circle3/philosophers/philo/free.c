2615f5a42cf68ee3a3ff811abfc2b96f63be36a8
username <ehdbs8747@naver.com>
2022-11-23 17:10:02 +0900

    Add philosophers/philo/free.c

---
+++ 42cursus/InnerCircle/circle3/philosophers/philo/free.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcakay <mcakay@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/12 01:43:25 by mcakay            #+#    #+#             */
+/*   Updated: 2022/10/12 02:35:50 by mcakay           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	ft_free(t_philo *philo, pthread_mutex_t *forks, pthread_mutex_t *death)
+{
+	int	i;
+
+	i = 0;
+	while (i < philo->philo_nb)
+	{
+		pthread_mutex_destroy(philo[i].left_fork_mutex);
+		pthread_mutex_destroy(philo[i].right_fork_mutex);
+		i++;
+	}
+	pthread_mutex_destroy(philo->death);
+	free(forks);
+	free(death);
+	free(philo);
+}
