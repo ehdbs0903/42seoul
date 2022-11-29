33ec29c802883780a3c6d834d0b66a17e22cf3fd
username <ehdbs8747@naver.com>
2022-11-29 18:18:33 +0900

    Update philosophers/philo/free.c

--- 42cursus/InnerCircle/circle3/philosophers/philo/free.c
+++ 42cursus/InnerCircle/circle3/philosophers/philo/free.c
@@ -6,25 +6,23 @@
 /*   By: doykim <doykim@student.42.kr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 01:43:25 by doykim            #+#    #+#             */
-/*   Updated: 2022/11/28 16:56:23 by doykim           ###   ########.fr       */
+/*   Updated: 2022/11/29 13:26:48 by doykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	ft_free(t_philo *philo, pthread_mutex_t *forks, pthread_mutex_t *death)
+void	free_all(t_philo *philo, t_mutex *mutex, t_variable *variable)
 {
 	int	i;
 
-	i = 0;
-	while (i < philo->philo_nb)
-	{
-		pthread_mutex_destroy(philo[i].left_fork_mutex);
-		pthread_mutex_destroy(philo[i].right_fork_mutex);
-		i++;
-	}
-	pthread_mutex_destroy(philo->death);
-	free(forks);
-	free(death);
+	i = -1;
+	while (++i < variable->num_of_philos)
+		pthread_join(philo[i].philo_tid, NULL);
+	i = -1;
+	pthread_mutex_destroy(&mutex->mutex_print);
+	while (++i < variable->num_of_philos)
+		pthread_mutex_destroy(&mutex->mutex_forks[i]);
+	free(mutex->mutex_forks);
 	free(philo);
 }
