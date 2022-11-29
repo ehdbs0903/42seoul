4015ca74ab95fca08d75c299bd26d215450b8e0c
username <ehdbs8747@naver.com>
2022-11-29 18:20:08 +0900

    Update philosophers/philo/utils.c

--- 42cursus/InnerCircle/circle3/philosophers/philo/utils.c
+++ 42cursus/InnerCircle/circle3/philosophers/philo/utils.c
@@ -6,34 +6,48 @@
 /*   By: doykim <doykim@student.42.kr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 01:04:08 by doykim            #+#    #+#             */
-/*   Updated: 2022/11/28 16:55:56 by doykim           ###   ########.fr       */
+/*   Updated: 2022/11/29 16:13:16 by doykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-long	ft_atol(const char *str)
+void	ft_usleep(t_philo *philo, int time, int timestamp)
 {
-	int		i;
-	long	nb;
-	int		sign;
+	int	time_taken;
 
-	i = 0;
-	nb = 0;
+	time_taken = 0;
+	while (time_taken <= timestamp + time * 1000)
+	{
+		time_taken = ret_timestamp(philo);
+		usleep(time);
+	}
+}
+
+int	ft_isdigit(int num)
+{
+	if (num >= '0' && num <= '9')
+		return (1);
+	else
+		return (0);
+}
+
+int	ft_atoi(const char *str)
+{
+	unsigned int	result;
+	int				sign;
+
 	sign = 1;
-	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == '\v'
-		|| str[i] == '\f' || str[i] == '\r')
-		i++;
-	if (str[i] == '-' || str[i] == '+')
+	result = 0;
+	while ((*str >= 9 && *str <= 13) || *str == ' ')
+		str++;
+	if (*str == '-' || *str == '+')
+		if (*str++ == '-')
+			sign *= -1;
+	while (*str >= '0' && *str <= '9')
 	{
-		if (str[i] == '-')
-			sign = -1;
-		i++;
+		result *= 10;
+		result += *str++ - '0';
 	}
-	while (str[i] >= '0' && str[i] <= '9')
-	{
-		nb = nb * 10 + str[i] - '0';
-		i++;
-	}
-	return (nb * sign);
+	return (result * sign);
 }
