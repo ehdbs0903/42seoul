beb1f805db60e37974a31b65ada0934b61bf3ad4
username <ehdbs8747@naver.com>
2022-12-20 12:44:04 +0900

    Update philosophers/philo/init.c

--- 42cursus/InnerCircle/circle3/philosophers/philo/init.c
+++ 42cursus/InnerCircle/circle3/philosophers/philo/init.c
@@ -3,79 +3,117 @@
 /*                                                        :::      ::::::::   */
 /*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: doykim <doykim@student.42seoul.kr>         +#+  +:+       +#+        */
+/*   By: doykim   <doykim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/28 18:16:20 by doykim            #+#    #+#             */
-/*   Updated: 2022/11/29 15:27:50 by doykim           ###   ########.fr       */
+/*   Created: 2022/11/19 15:57:03 by doykim            #+#    #+#             */
+/*   Updated: 2022/12/19 19:53:48 by doykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	create_thread(t_philo *philo, int num, int num_of_philos)
+int	philo_atoi(const char *str)
+{
+	int			i;
+	long long	res;
+
+	i = 0;
+	res = 0;
+	while ((9 <= str[i] && str[i] <= 13) || (str[i] == 32))
+		i++;
+	if (str[i] == '+')
+		i++;
+	while (str[i])
+	{
+		if (str[i] >= '0' && str[i] <= '9')
+		{
+			res = (res * 10) + (str[i] - '0');
+			if (res > 2147483647)
+				return (-1);
+		}
+		else
+			return (-1);
+		i++;
+	}
+	return ((int)res);
+}
+
+int	init_info(t_info *info, int argc, char **argv)
 {
 	int	i;
 
-	i = num;
-	while (i < num_of_philos)
-	{
-		philo[i].philo_number = i;
-		pthread_create(&philo[i].philo_tid, NULL, thread_philo, &philo[i]);
-		i += 2;
-	}
-}
-
-void	init_varialbe(int ac, char **av, t_variable *variable)
-{
-	if (ac == 5)
-		variable->must_eat = -1;
+	info->num_of_philo = philo_atoi(argv[1]);
+	info->time_to_die = philo_atoi(argv[2]);
+	info->time_to_eat = philo_atoi(argv[3]);
+	info->time_to_sleep = philo_atoi(argv[4]);
+	if (argc == 6)
+		info->must_eat_count = philo_atoi(argv[5]);
 	else
-		variable->must_eat = ft_atoi(av[5]);
-	variable->num_of_philos = ft_atoi(av[1]);
-	variable->time_to_die = ft_atoi(av[2]);
-	variable->time_to_eat = ft_atoi(av[3]);
-	variable->time_to_sleep = ft_atoi(av[4]);
-	variable->finished_meal = 0;
-	variable->philo_alive = 1;
-	variable->first_meal_time.tv_sec = 0;
-	variable->first_meal_time.tv_usec = 0;
+		info->must_eat_count = -1;
+	if (!(check_info_value(info, argc)))
+		return (return_error("argument error"));
+	info->all_alive = 1;
+	info->forks = \
+		(pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * info->num_of_philo);
+	if (!(info->forks))
+		return (return_error("malloc error"));
+	i = -1;
+	while (++i < info->num_of_philo)
+		pthread_mutex_init(&(info->forks[i]), NULL);
+	pthread_mutex_init(&(info->guard), NULL);
+	pthread_mutex_init(&(info->print), NULL);
+	return (1);
 }
 
-void	init_mutex(t_mutex *mutex, t_variable *variable)
+int	check_info_value(t_info *info, int argc)
+{
+	if (info->num_of_philo <= 0 || \
+		info->time_to_die <= 0 || \
+		info->time_to_eat <= 0 || \
+		info->time_to_sleep <= 0 || \
+		((argc == 6) && (info->must_eat_count <= 0)))
+		return (0);
+	else
+		return (1);
+}
+
+int	init_philo(t_philo **philo, t_info *info)
 {
 	int	i;
 
-	i = -1;
-	mutex->mutex_forks = malloc(sizeof(pthread_mutex_t) * \
-		variable->num_of_philos);
-	if (mutex->mutex_forks == NULL)
-		print_error(2);
-	while (++i < variable->num_of_philos)
-		pthread_mutex_init(&mutex->mutex_forks[i], NULL);
-	pthread_mutex_init(&mutex->mutex_print, NULL);
+	*philo = (t_philo *)malloc(sizeof(t_philo) * info->num_of_philo);
+	if (!(*philo))
+	{
+		free_info_malloc(info);
+		return (return_error("argument error"));
+	}
+	i = 0;
+	while (i < info->num_of_philo)
+	{
+		(*philo)[i].info = info;
+		(*philo)[i].id = i + 1;
+		pthread_mutex_init(&((*philo)[i].guard), NULL);
+		(*philo)[i].left_fork = i;
+		(*philo)[i].right_fork = (i + 1) % info->num_of_philo;
+		(*philo)[i].eat_time = get_time();
+		(*philo)[i].eat_count = 0;
+		(*philo)[i].finish = 0;
+		i++;
+	}
+	return (1);
 }
 
-t_philo	*init_philos(t_variable *variable, t_mutex *mutex)
+void	free_info_malloc(t_info *info)
 {
-	int		i;
-	t_philo	*philo;
+	int	i;
 
-	i = -1;
-	philo = malloc(sizeof(t_philo) * variable->num_of_philos);
-	if (philo == NULL)
-		print_error(2);
-	while (++i < variable->num_of_philos)
+	i = 0;
+	while (info->num_of_philo)
 	{
-		philo[i].mutex = mutex;
-		philo[i].variable = variable;
-		philo[i].have_meal = 0;
-		philo[i].last_meal_time = 0;
-		philo[i].fork_number[RIGHT] = i;
-		philo[i].fork_number[LEFT] = -1;
-		if (i + 1 != variable->num_of_philos)
-			philo[i].fork_number[LEFT] = i + 1;
-		else if (i != 0)
-			philo[i].fork_number[LEFT] = 0;
+		pthread_mutex_destroy(&(info->forks[i]));
+		i++;
 	}
-	return (philo);
+	free(info->forks);
+	pthread_mutex_destroy(&(info->guard));
+	pthread_mutex_destroy(&(info->print));
 }
