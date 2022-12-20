1309bd0cddbd0a519ce1c657fe80abba6be524e9
username <ehdbs8747@naver.com>
2022-12-20 12:43:38 +0900

    Update philosophers/philo/free.c

--- 42cursus/InnerCircle/circle3/philosophers/philo/free.c
+++ 42cursus/InnerCircle/circle3/philosophers/philo/free.c
@@ -3,26 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   free.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: doykim <doykim@student.42.kr>              +#+  +:+       +#+        */
+/*   By: doykim <doykim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/23 01:43:25 by doykim            #+#    #+#             */
-/*   Updated: 2022/11/29 13:26:48 by doykim           ###   ########.fr       */
+/*   Created: 2022/11/25 16:49:27 by doykim            #+#    #+#             */
+/*   Updated: 2022/12/19 19:52:01 by doykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	free_all(t_philo *philo, t_mutex *mutex, t_variable *variable)
+void	free_all(t_philo **philo, t_info *info)
 {
 	int	i;
 
-	i = -1;
-	while (++i < variable->num_of_philos)
-		pthread_join(philo[i].philo_tid, NULL);
-	i = -1;
-	pthread_mutex_destroy(&mutex->mutex_print);
-	while (++i < variable->num_of_philos)
-		pthread_mutex_destroy(&mutex->mutex_forks[i]);
-	free(mutex->mutex_forks);
-	free(philo);
+	i = 0;
+	while (i < info->num_of_philo)
+	{
+		pthread_join((*philo)[i].thread, NULL);
+		i++;
+	}
+	i = 0;
+	while (i < info->num_of_philo)
+	{
+		pthread_mutex_destroy(&(info->forks[i]));
+		pthread_mutex_destroy(&((*philo)[i].guard));
+		i++;
+	}
+	pthread_mutex_destroy(&(info->guard));
+	pthread_mutex_destroy(&(info->print));
+	free(info->forks);
+	free(*philo);
 }
