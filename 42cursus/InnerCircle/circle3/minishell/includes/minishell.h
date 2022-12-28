f55fc8b849209ca02c01ceeab98f087c4e09e84e
username <ehdbs8747@naver.com>
2022-12-29 03:03:26 +0900

    Add minishell/includes/minishell.h

--- 42cursus/InnerCircle/circle3/minishell/includes/minishell.h
+++ 42cursus/InnerCircle/circle3/minishell/includes/minishell.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: doykim <doykim@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/22 19:57:50 by doykim            #+#    #+#             */
+/*   Updated: 2022/12/23 03:09:26 by doykim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
@@ -8,6 +20,27 @@
 # include <fcntl.h>
 # include "../libs/libft/libft.h"
 
+# define TRUE 1
+# define FALSE 0
 
+# define CMD 1
+# define WORD 2
+# define PIPE 3
+# define REDIRECT 4
+
+typedef struct	s_parser_tokens
+{
+	t_list	*type;
+	t_list	*cmd;
+	t_list	*word;
+	t_list	*pipe;
+	t_list	*redirect;
+}	t_parser_tokens;
+
+void	lexer(char *str, t_list **lexer_tokens);
+
+void	parser(t_list *lexer_token, t_parser_tokens *parser_tokens);
+
+int iscmd(char *str);
 
 #endif
