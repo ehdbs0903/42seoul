1a087b3a9123056b795d4287842a01880df5a7ee
username <ehdbs8747@naver.com>
2023-01-25 21:14:54 +0900

    Update cub3d/srcs/init/init_game.c

--- 42cursus/InnerCircle/circle4/cub3d/srcs/init/init_game.c
+++ 42cursus/InnerCircle/circle4/cub3d/srcs/init/init_game.c
@@ -6,7 +6,7 @@
 /*   By: doykim <doykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 19:48:22 by doykim            #+#    #+#             */
-/*   Updated: 2023/01/19 21:02:32 by doykim           ###   ########.fr       */
+/*   Updated: 2023/01/25 21:06:11 by doykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,12 @@ void	init_game(int ac, char *av[], t_game *game)
 	game->so_path = NULL;
 	game->we_path = NULL;
 	game->ea_path = NULL;
-	game->floor.r = 0;
-	game->floor.g = 0;
-	game->floor.b = 0;
-	game->ceil.r = 0;
-	game->ceil.g = 0;
-	game->ceil.b = 0;
+	game->floor = NULL;
+	game->ceil = NULL;
 	game->player.x = 0;
 	game->player.y = 0;
 	game->player.dir = 0;
-	check_file(ac, av, game);
 	game->mlx = mlx_init();
+	check_file(ac, av, game);
 	game->win = mlx_new_window(game->mlx, 1920, 1080, "cub3d");
 }
