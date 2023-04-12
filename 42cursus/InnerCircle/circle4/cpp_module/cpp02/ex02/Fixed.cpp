2e6a2d8817597ed482d967d84856b456b93689f0
username <ehdbs8747@naver.com>
2023-04-12 19:18:10 +0900

    Update ex02/Fixed.cpp

--- 42cursus/InnerCircle/circle4/cpp_module/cpp02/ex02/Fixed.cpp
+++ 42cursus/InnerCircle/circle4/cpp_module/cpp02/ex02/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: doykim <doykim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 13:56:57 by doykim            #+#    #+#             */
-/*   Updated: 2023/03/22 21:18:32 by doykim           ###   ########.fr       */
+/*   Updated: 2023/03/30 15:52:37 by doykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,28 @@
 
 Fixed::Fixed(void) 
 {
-	std::cout << "Default constructor called" << std::endl;
 	this->value = 0;
 }
 
 Fixed::Fixed(int n)
 {
-	std::cout << "Int constructor called" << std::endl;
 	this->value = n << this->bits;
 }
 
 Fixed::Fixed(float n)
 {
-	std::cout << "Float constructor called" << std::endl;
 	this->value = roundf(n * (1 << this->bits));
 }
 
 Fixed::Fixed(const Fixed& obj) 
 {
-	std::cout << "Copy constructor called" << std::endl;
 	*this = obj;
 }
 
-Fixed::~Fixed(void) 
-{
-	std::cout << "Destructor called" << std::endl;
-}
+Fixed::~Fixed(void) {}
 
 Fixed& Fixed::operator=(const Fixed& obj) 
 {
-	std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &obj)
 	{
 		this->value = obj.getRawBits();
